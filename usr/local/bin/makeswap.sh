0682b71cdad954a00603c3b5a4aa2837a2bacb78
ppdac <law2175@columbia.edu>
2020-03-07 20:42:56 +0000

    Users can set desired swap size in /var/local/makeswap-on-azure/swap_size

--- usr/local/bin/makeswap.sh
+++ usr/local/bin/makeswap.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+#sdb is already mounted at /mnt so let's use it
 FILE=/mnt/swapfile
+
+# Set /var/local/makeswap-on-azure/swap_size to your desired size.
+# For example sudo su; echo 3600M > /var/local/makeswap-on-zure/swap_size; exit;
+# It can be something like:
+# 1024K
+# 1024M
+# 3.5G
+# The default is 3333M
+SWAP_SIZE=$(</var/local/makeswap-on-azure/swap_size)
+
+
+#This writes do dmesg, but not enough.
 if test -f "$FILE"; then
     echo "makeswap-on-azure-vm-tmp-drive: swapfile file alreaedy exists." >  /dev/kmsg
 else
-    fallocate -l 3333M $FILE
+    fallocate -l $SWAP_SIZE $FILE
     chmod 600 $FILE
     mkswap $FILE
     swapon $FILE
