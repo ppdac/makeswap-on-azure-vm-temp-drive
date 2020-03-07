c7bb0d7e2a053c79997171a620dc84b9f1fcd77a
Ubuntu <louis@cosmic.y42b43zzdhuubctgtq4pa1e5rb.jx.internal.cloudapp.net>
2020-03-07 15:46:12 +0000

    Initial commit with hardcoded swapsize.

---
+++ usr/local/bin/makeswap.sh
@@ -0,0 +1,9 @@
+FILE=/mnt/swapfile
+if test -f "$FILE"; then
+    echo "make-swap-file-on-azure-vm-tmp-drive: swap file alreaedy exists." >  /dev/kmsg
+else
+    fallocate -l 3333M $FILE
+    chmod 600 $FILE
+    mkswap $FILE
+    swapon $FILE
+fi  
