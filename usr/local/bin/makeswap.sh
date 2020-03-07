42593f6de6870bda7504d6c50778189c16eb8fdd
Louis Waweru <law2175@columbia.edu>
2020-03-07 17:33:18 -0500

    don't reset SWAP_SIZE if it's already set

--- usr/local/bin/makeswap.sh
+++ usr/local/bin/makeswap.sh
@@ -3,24 +3,41 @@
 #sdb is already mounted at /mnt so let's use it
 FILE=/mnt/swapfile
 
-#The parameter needs to exist.
-mkdir -p /var/local/makeswap-on-azure
-touch /var/local/makeswap-on-azure/swap_size
-echo 3333M > /var/local/makeswap-on-azure/swap_size
 
-# Set /var/local/makeswap-on-azure/swap_size to your desired size.
+#The parameter needs to exist.
+PARAMETER_FILE=/var/local/makeswap-on-azure/swap_size
+
+if test -f $PARAMETER_FILE; then
+    #Use the value from the parameter file if it exists
+    SWAP_SIZE=$(<$PARAMETER_FILE)
+else
+    #Otherwise make one with a reasonable value
+    mkdir -p $PARAMETER_FILE
+    touch $PARAMETER_FILE
+    echo 3333M > $PARAMETER_FILE
+fi
+
+# Set /var/local/makeswap-on-azure/swap_size to your desired size and restart service.
 # For example sudo su; echo 3600M > /var/local/makeswap-on-zure/swap_size; exit;
 # It can be something like:
 # 1024K
 # 1024M
 # 3.5G
 # The default is 3333M
-SWAP_SIZE=$(</var/local/makeswap-on-azure/swap_size)
+SWAP_SIZE=$(<$PARAMETER_FILE)
 
 
 #This writes do dmesg, but not enough.
 if test -f "$FILE"; then
-    echo "makeswap-on-azure-vm-tmp-drive: swapfile file alreaedy exists." >  /dev/kmsg
+    echo "makeswap-on-azure-vm-tmp-drive: swapfile file alreaedy exists." > /dev/kmsg
+    swapoff $FILE
+    rm $FILE
+    echo "makeswap-on-azure-vm-tmp-drive: Deleted it." > /dev/kmsg
+    
+    fallocate -l $SWAP_SIZE $FILE
+    chmod 600 $FILE
+    mkswap $FILE
+    swapon $FILE
 else
     fallocate -l $SWAP_SIZE $FILE
     chmod 600 $FILE
