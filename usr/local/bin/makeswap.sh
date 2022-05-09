14fa13e63ee913c64ece289e672c8a8e2fe14ef3
Louis Waweru <louis@waweru.me>
2022-05-09 16:26:54 -0400

    MOS: capitalize constants; headers; function desc

--- usr/local/bin/makeswap.sh
+++ usr/local/bin/makeswap.sh
@@ -1,82 +1,107 @@
 #!/bin/bash
 #
-# Creates swapfile on the temp drive
+# Creates swap file on the temp drive
 
 # Use the value from the parameter file if it exists
 # You can always set /var/local/makeswap-on-azure/swap_size to your desired size and restart service.
-readonly parameterFile='/var/local/makeswap-on-azure/swap_size'
-readonly swapFile='/mnt/pagefile'
+readonly PARAMETER_FILE='/var/local/makeswap-on-azure/swap_size'
+readonly SWAP_FILE='/mnt/pagefile'
 
-# Logging function
+# Usage: logger "This will be logged."
 logger() {
     echo "[makeswap-on-azure]: $*" >&1
 }
 
-# STDERR logging function
+# Usage: err "This is sent to STDERR before returning 1 on exit."
 err() {
     echo "[makeswap-on-azure]: $*" >&2
     exit 1
 }
 
+#######################################
 # Allocates disk space, creates swap file and enables it
+# Globals:
+#   PARAMETER_FILE
+#   SWAP_FILE
+# Arguments:
+#   None
+# Returns:
+#   0 or non-zero on error.
+#######################################
 commit_swap() {
     # https://github.com/ppdac/makeswap-on-azure.service/issues/3
-    chmod ugo+w "$parameterFile"
-    local swapSize=$(cat $parameterFile)
-    fallocate -l $swapSize $swapFile
+    chmod ugo+w "$PARAMETER_FILE"
+    local readonly SWAP_SIZE=$(cat $PARAMETER_FILE)
+    fallocate -l $SWAP_SIZE $SWAP_FILE
     if (($? == 0)); then
-        logger "Allocated $swapSize for $swapFile"
+        logger "Allocated $SWAP_SIZE for $SWAP_FILE"
     else
-        err "Failed to allocate $swapSize to $swapFile"
+        err "Failed to allocate $SWAP_FILE to $SWAP_FILE"
     fi
 
-    chmod 600 "$swapFile"
+    chmod 600 "$SWAP_FILE"
     if (($? == 0)); then
-        logger "Set 600 permissions on $swapFile"
+        logger "Set 600 permissions on $SWAP_FILE"
     else
-        err "Failed to set 600 permissions on $swapFile"
+        err "Failed to set 600 permissions on $SWAP_FILE"
     fi
 
-    mkswap "$swapFile"
+    mkswap "$SWAP_FILE"
     if (($? == 0)); then
-        logger "Recreated swap file $swapFile"
+        logger "Recreated swap file $SWAP_FILE"
     else
-        err "Failed to recreate swap file $swapFile"
+        err "Failed to recreate swap file $SWAP_FILE"
     fi
 
-    swapon $swapFile
+    swapon $SWAP_FILE
     if (($? == 0)); then
-        logger "The ${swapSize} swapfile was enabled as $swapFile."
+        logger "The ${SWAP_FILE} swap file was enabled as $SWAP_FILE."
     else
-        err "Failed to enable the swap file $swapFile"
+        err "Failed to enable the swap file $SWAP_FILE"
     fi
 }
 
-remove_swap()
-{
+#######################################
+# Turns off he swap function and deletes the swap file
+# Globals:
+#   PARAMETER_FILE
+# Arguments:
+#   None
+# Returns:
+#   0 or non-zero on error.
+#######################################
+remove_swap() {
     # https://github.com/ppdac/makeswap-on-azure.service/issues/3
-    chmod ugo+w "$parameterFile"
+    chmod ugo+w "$PARAMETER_FILE"
 
-    if [ -f "$swapFile"] ; then
+    if [ -f "$SWAP_FILE"] ; then
     logger "Swap file already exists, removing it."
-    swapoff "$swapFile"
+    swapoff "$SWAP_FILE"
     if (($? == 0)); then
         logger "Swap disabled."
     else
         err  "Failed to disable swap."
     fi
     
-    rm $swapFile
+    rm $SWAP_FILE
     if (($? == 0)); then
-        logger "$swapFile was removed."
+        logger "$SWAP_FILE was removed."
     else
-        err "Failed to remove swap file from $swapFile"
+        err "Failed to remove swap file from $SWAP_FILE"
     fi
-fi
 }
 
+#######################################
+# Calculate size of swap appropriate with respect to space and memory
+# Globals:
+#   PARAMETER_FILE
+# Arguments:
+#   None
+# Returns:
+#   0 if calculation completed, non-zero on error.
+#######################################
 calculate_swap_size() {
-    local readonly filesystem=$(df | grep /mnt | gawk '{print $1}')
+    local readonly FILESYSTEM=$(df | grep /mnt | gawk '{print $1}')
 
     # Azure won't give you the full amount of RAM as some of it is taken by platform services.
     # Approximating 512 MiB to 512 * 1024, and so on, is close enough for these increments
@@ -85,63 +110,69 @@ calculate_swap_size() {
     logger "Total RAM: ${memTotal}M."
 
     # Kilobyes/1024+0.5 is good for general rounding, but we don't want more than is available
-    local freeDiskSpace=$(df | grep /mnt | gawk '{print int($4/1024)}')
-    logger "$freeDiskSpace megabyes available on $filesystem (rounding down for safety)."
+    local FREE_DISK_SPACE=$(df | grep /mnt | gawk '{print int($4/1024)}')
+    logger "$FREE_DISK_SPACE megabyes available on $FILESYSTEM (rounding down for safety)."
 
-    if [ $freeDiskSpace -gt 0 ]; then
-        logger "${freeDiskSpace}M available disk space on $filesystem."
+    if [ $FREE_DISK_SPACE -gt 0 ]; then
+        logger "${FREE_DISK_SPACE}M available disk space on $FILESYSTEM."
     else
-        err "{freeDiskSpace} bytes is not enough to proceed. Please free up some space on $filesystem."
+        err "{FREE_DISK_SPACE} bytes is not enough to proceed. Please free up some space on $FILESYSTEM."
     fi
 
     # Fork me ¯\_(ツ)_/¯  
-    if [ $freeDiskSpace -ge 256 ]; then
+    if [ $FREE_DISK_SPACE -ge 256 ]; then
         if [ $memTotal -lt 512 ]; then
-            echo 256M > $parameterFile
-        elif [ $memTotal -le 1024 ] && [ 512 -le $freeDiskSpace ];then
-            echo 512M > $parameterFile
-        elif [ $memTotal -le  2048 ] && [ 1024 -le $freeDiskSpace ]; then
-            echo 1G > $parameterFile
-        elif [ $memTotal -le 3072 ] && [ 1024 -le $freeDiskSpace ]; then
-            echo 1G > $parameterFile
-        elif [ $memTotal -le 4096 ] && [ 2048 -le $freeDiskSpace ]; then
-            echo 2G > $parameterFile
-        elif [ $memTotal -le 6144 ] && [ 4096 -le $freeDiskSpace ]; then
-            echo 4G > $parameterFile
-        elif [ $memTotal -le 7168 ] && [ 6144 -le $freeDiskSpace ]; then
-            echo 6G > $parameterFile
-        elif [ $memTotal -le 8192 ] && [ 7168 -le $freeDiskSpace ]; then
-            echo 7G > $parameterFile
-        elif [ $memTotal -le 12288 ] && [ 8192 -le $freeDiskSpace ]; then
-            echo 8G > $parameterFile
-        elif [ $memTotal -le 16384 ] && [ 12288 -le $freeDiskSpace ]; then
-            echo 12G > $parameterFile
-        elif [ $memTotal -ge 16384 ] && [ 16384 -le $freeDiskSpace ]; then
-            echo 16G > $parameterFile
-        elif [ $memTotal -le $freeDiskSpace ]; then
-            echo "${freeDiskSpace}M" > $parameterFile
+            echo 256M > $PARAMETER_FILE
+        elif [ $memTotal -le 1024 ] && [ 512 -le $FREE_DISK_SPACE ];then
+            echo 512M > $PARAMETER_FILE
+        elif [ $memTotal -le 3072 ] && [ 1024 -le $FREE_DISK_SPACE ]; then
+            echo 1G > $PARAMETER_FILE
+        elif [ $memTotal -le 4096 ] && [ 2048 -le $FREE_DISK_SPACE ]; then
+            echo 2G > $PARAMETER_FILE
+        elif [ $memTotal -le 6144 ] && [ 4096 -le $FREE_DISK_SPACE ]; then
+            echo 4G > $PARAMETER_FILE
+        elif [ $memTotal -le 7168 ] && [ 6144 -le $FREE_DISK_SPACE ]; then
+            echo 6G > $PARAMETER_FILE
+        elif [ $memTotal -le 8192 ] && [ 7168 -le $FREE_DISK_SPACE ]; then
+            echo 7G > $PARAMETER_FILE
+        elif [ $memTotal -le 12288 ] && [ 8192 -le $FREE_DISK_SPACE ]; then
+            echo 8G > $PARAMETER_FILE
+        elif [ $memTotal -le 16384 ] && [ 12288 -le $FREE_DISK_SPACE ]; then
+            echo 12G > $PARAMETER_FILE
+        elif [ $memTotal -ge 16384 ] && [ 16384 -le $FREE_DISK_SPACE ]; then
+            echo 16G > $PARAMETER_FILE
+        elif [ $memTotal -le $FREE_DISK_SPACE ]; then
+            echo "${FREE_DISK_SPACE}M" > $PARAMETER_FILE
         else
             err "Failed to calculate swap size."
         fi
     fi
 
-    logger "Calculated swap size of $(cat $parameterFile)."
+    logger "Calculated swap size of $(cat $PARAMETER_FILE)."
 }
 
+#######################################
+# Program flow structure. Called from final line.
+# Globals:
+#   PARAMETER_FILE
+#   SWAP_FILE
+# Arguments:
+#   None
+#######################################
 main() {
-    if [ -f "$parameterFile" ]; then
-        logger "Found $parameterFile."
-        if [[ ! -z $parameterFile ]]; then
+    if [ -f "$PARAMETER_FILE" ]; then
+        logger "Found $PARAMETER_FILE."
+        if [[ ! -z $PARAMETER_FILE ]]; then
             # Set the size of swap using the value stored in /var/local/makeswap-on-azure/swap_size
-            swapSize=$(cat $parameterFile)
-            logger "Set the swap size to $swapSize"
+            local readonly SWAP_SIZE=$(cat $PARAMETER_FILE)
+            logger "Set the swap size to $SWAP_SIZE"
         else
-            err "$parameterFile is not set."
+            err "$PARAMETER_FILE is not set."
         fi
     fi
 
-    if [ ! -f $parameterFile ] || [ -z $swapSize ]; then
-        logger "$parameterFile is not present or is not set."
+    if [ ! -f $PARAMETER_FILE ] || [ -z $SWAP_SIZE ]; then
+        logger "$PARAMETER_FILE is not present or is not set."
 
         mkdir -p "/var/local/makeswap-on-azure"
         if (($? == 0)); then
@@ -150,14 +181,14 @@ main() {
             err "Failed create directory /var/local/makeswap-on-azure"
         fi
 
-        touch $parameterFile
+        touch $PARAMETER_FILE
         if (($? == 0)); then
-            logger "Created $parameterFile to hold swap size configuration."
+            logger "Created $PARAMETER_FILE to hold swap size configuration."
         else
-            err "Failed to create configuration file $parameterFile"
+            err "Failed to create configuration file $PARAMETER_FILE"
         fi
 
-        if [ -f $swapFile ]; then
+        if [ -f $SWAP_FILE ]; then
             logger "Removing a swap file that is already present."
             remove_swap
             if (($? != 0)); then
